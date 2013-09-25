477b80dfdfcf59a149fed6e239a6fd1b04cb921e
Kaspars Dambis <kaspars@konstruktors.com>
2013-09-25 15:01:42 +0300

    Add username to logged in variable

--- ga-admin-exclude.php
+++ ga-admin-exclude.php
@@ -4,14 +4,19 @@
 	Description: Sets a <code>logged_in</code> variable when a user is logged in.
 	Author: Kaspars Dambis
 	Author URL: http://kaspars.net
-	Version: 1.0
+	Version: 1.1
 */
 
 add_action( 'wp_footer', 'ga_admin_exclude' );
 
 function ga_admin_exclude() {
+	global $current_user;
+
 	if ( is_user_logged_in() )
-		echo '<script type="text/javascript">var _gaq = _gaq || []; _gaq.push(["_setVar", "logged_in"]);</script>';
+		printf( 
+			'<script type="text/javascript">var _gaq = _gaq || []; _gaq.push(["_setCustomVar", 1, "logged_in", "%s"]);</script>',
+			$current_user->user_login 
+		);
 }
 
 
